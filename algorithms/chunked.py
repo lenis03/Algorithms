99893c68b9d1c3b75b44b8eda31dceaf06fbdc60
Lenis <fardintorkamand83@gmail.com>
2024-06-15 20:59:48 +0330

    Add docstring for chunked algorithm & add init file

--- algorithms/chunked.py
+++ algorithms/chunked.py
@@ -1,14 +1,28 @@
 from functools import partial
 from itertools import islice
 
-lst = [i for i in range(9)]
-
 
 def take(iterable, n):
     return list(islice(iterable, n))
 
 
 def chunked(iterable, n, strict=False):
+    """Break *iterable* into lists of length *n*:
+
+    >>> list(chunked([1, 2, 3, 4, 5, 6], 3))
+    [[1, 2, 3], [4, 5, 6]]
+
+    By the default, the last yielded list will have fewer than *n* elements
+    if the length of *iterable* is not divisible by *n*:
+
+        >>> list(chunked([1, 2, 3, 4, 5, 6, 7, 8], 3))
+        [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+    If the length of *iterable* is not divisible by *n* and *strict* is
+    ``True``, then ``ValueError`` will be raised before the last
+    list is yielded.
+
+    """
     iterator = iter(partial(take, iter(iterable), n), [])
 
     if strict:
@@ -24,6 +38,3 @@ def chunked(iterable, n, strict=False):
         return iter(ret())
 
     return iterator
-
-print(lst)
-print(list(chunked(lst, 3, True)))
