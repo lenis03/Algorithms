66eebbdb52988e5fe2a5cab83f9139d616f2fedc
Lenis <fardintorkamand83@gmail.com>
2024-06-22 21:24:42 +0330

    Write tests for interleave algorithm

--- tests/test_more.py
+++ tests/test_more.py
@@ -9,7 +9,8 @@ from algorithms.more import (
     first,
     last,
     nth_or_last,
-    one
+    one,
+    interleave,
 
 )
 
@@ -203,6 +204,26 @@ class OneTest(unittest.TestCase):
             'and perhaps more.',
             lambda: one(it)
         )
+
+
+class InterleaveTest(unittest.TestCase):
+    def test_even(self):
+        actual = list((interleave([1, 4, 7], [2, 5, 8], [3, 6, 9])))
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(actual, expected)
+
+    def test_short(self):
+        actual = list((interleave([1, 4, 7], [2, 5, 8], [3, 6])))
+        expected = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(actual, expected)
+
+    def test_mixed_types(self):
+        it_list = ['a', 'b', 'c', 'd']
+        it_str = '123456'
+        it_inf = count()
+        actual = list(interleave(it_list, it_str, it_inf))
+        expected = ['a', '1', 0, 'b', '2', 1, 'c', '3', 2, 'd', '4', 3]
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
