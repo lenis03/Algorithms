1d7bf36f3c8f01b0e13cef8281ab4264157d866b
Lenis <fardintorkamand83@gmail.com>
2024-07-11 14:13:19 +0330

    Write tests for split after algorithm

--- tests/test_more.py
+++ tests/test_more.py
@@ -16,7 +16,7 @@ from algorithms.more import (
     only,
     always_reversible,
     always_iterable,
-
+    split_after,
 
 )
 
@@ -456,6 +456,53 @@ class AlwaysIterableTest(unittest.TestCase):
             yield 0
             yield 1
         self.assertEqual(list(always_iterable(_gen())), [0, 1])
+
+
+class SplitAfterTest(unittest.TestCase):
+    def test_start_with_sep(self):
+        actual = list(split_after('xooxoo', lambda c: c == 'x'))
+        expected = [['x'], ['o', 'o', 'x'], ['o', 'o']]
+        self.assertEqual(actual, expected)
+
+    def test_ends_with_sep(self):
+        actual = list(split_after('ooxoox', lambda c: c == 'x'))
+        expected = [['o', 'o', 'x'], ['o', 'o', 'x']]
+        self.assertEqual(actual, expected)
+
+    def test_no_sep(self):
+        actual = list(split_after('ooo', lambda c: c == 'x'))
+        expected = [['o', 'o', 'o']]
+        self.assertEqual(actual, expected)
+
+    def test_max_split(self):
+        for args, expected in [
+            (
+                ('a,b,c,d', lambda c: c == ',', -1),
+                [['a', ','], ['b', ','], ['c', ','], ['d']]
+            ),
+            (
+                ('a,b,c,d', lambda c: c == ',', 0),
+                [['a', ',', 'b', ',', 'c', ',', 'd']]
+            ),
+            (
+                ('a,b,c,d', lambda c: c == ',', 1),
+                [['a', ','], ['b', ',', 'c', ',', 'd']]
+            ),
+            (
+                ('a,b,c,d', lambda c: c == ',', 2),
+                [['a', ','], ['b', ','], ['c', ',', 'd']]
+            ),
+            (
+                ('a,b,c,d', lambda c: c == ',', 10),
+                [['a', ','], ['b', ','], ['c', ','], ['d']]
+            ),
+            (
+                ('a,b,c,d', lambda c: c == '@', 2),
+                [['a', ',', 'b', ',', 'c', ',', 'd']]
+            )
+        ]:
+            actual = list(split_after(*args))
+            self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
