3d05258ac167e1befa5e3977e3398c087503ee48
Lenis <fardintorkamand83@gmail.com>
2024-07-12 09:14:45 +0330

    Implement SequenceView algorithm

--- algorithms/more.py
+++ algorithms/more.py
@@ -668,3 +668,21 @@ def value_chain(*args):
 
         except TypeError:
             yield value
+
+
+class SequenceView(Sequence):
+
+    def __init__(self, target):
+        if not isinstance(target, Sequence):
+            raise TypeError
+
+        self._target = target
+
+    def __getitem__(self, index):
+        return self._target[index]
+
+    def __len__(self):
+        return len(self._target)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self._target})'
